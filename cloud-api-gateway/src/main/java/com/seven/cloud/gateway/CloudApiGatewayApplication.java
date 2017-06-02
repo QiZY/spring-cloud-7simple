62225bb00a83e63ffafe66eeec78fe47d1c8fa67
qizhiyong <283660826@qq.com>
2017-06-02 15:43:49 +0800

    zuul路由，拦截过滤

--- cloud-api-gateway/src/main/java/com/seven/cloud/gateway/CloudApiGatewayApplication.java
+++ cloud-api-gateway/src/main/java/com/seven/cloud/gateway/CloudApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.seven.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
@@ -10,6 +11,7 @@ import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class CloudApiGatewayApplication {
 
     /**
